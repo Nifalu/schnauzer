7571745076cefdd48193ffa7d529812becb883ea
Nifalu <n@nifalu.ch>
2025-06-20 14:39:12 +0200

    update version number once again

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="schnauzer",
-    version="0.2.0",
+    version="0.2.1",
     description="Visualize networkx graphs interactively in a web browser.",
     author="Nico Bachmann",
     author_email="python@deschnauz.ch",
